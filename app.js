79dbfa54391fdfce67a13f5ba18dbfe8a1970510
Mitchel Pawirodinomo <pawiromitchel@gmail.com>
2021-09-08 16:40:31 -0300

    progress bar added

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 var app = new Vue({
     el: '#app',
     data: {
+        progressValue: 0,
+        date: '',
         chains: [],
     }
 });
@@ -15,11 +17,29 @@ async function getGasPrices(network) {
     return movies;
 }
 
-chains.forEach(async chain => {
-    const req = getGasPrices(chain);
-    const res = await req;
-    app.chains.push({
-        name: capitalize(removeDashes(chain)),
-        gasPrice: res
-    })
-});
+function getData() {
+    app.date = new Date().toString();
+    app.chains = [];
+    chains.forEach(async chain => {
+        const req = getGasPrices(chain);
+        const res = await req;
+        app.chains.push({
+            name: capitalize(removeDashes(chain)),
+            gasPrice: res
+        })
+    });
+}
+
+getData();
+
+let counter = 25;
+const newYearCountdown = setInterval(function () {
+    counter--;
+    app.progressValue += 4;
+    if (counter === 0) {
+        getData();
+        // reset the values
+        app.progressValue = 0;
+        counter = 5;
+    }
+}, 1000);
