d9239ee146d26c6618c1827bbdea97268a057f70
Joel Naarendorp <joel.naarendorp.natin@gmail.com>
2021-09-09 01:21:58 -0300

    working version of app ported to vue3

--- app.js
+++ app.js
@@ -1,54 +1,60 @@
-// bootstrap Vue
-var app = new Vue({
-    el: '#app',
-    data: {
-        progressValue: 0,
-        date: '',
-        chains: [],
-    }
-});
 
 const endpoint = 'https://api.zapper.fi/v1/gas-price?api_key=96e0cc51-a62e-42ca-acee-910ea7d2a241&network=';
 const chains = ['ethereum', 'fantom', 'polygon', 'binance-smart-chain'];
 
-function boilerplate() {
-    chains.forEach(chain => {
-        app.chains.push({
-            name: capitalize(removeDashes(chain)),
-            gasPrice: {
-                instant: 0,
-                fast: 0,
-                standard: 0,
+// bootstrap Vue
+const GasTracker = {
+    data() {
+        return {
+            progressValue: 0,
+            date: '',
+            chains: [],
+            counter: 100
+        }
+    },
+    methods: {
+        getGasPrices: async (network) => {
+            const response = await fetch(`${endpoint}${network}`);
+            const prices = await response.json();
+            return prices;
+        },
+        boilerplate: () => {
+            chains.forEach(chain => {
+                app.chains.push({
+                    name: capitalize(removeDashes(chain)),
+                    gasPrice: {
+                        instant: 0,
+                        fast: 0,
+                        standard: 0,
+                    }
+                })
+            })
+        },
+        getData: async () => {
+            app.date = new Date().toString();
+            const data = await Promise.all(chains.map(chain => app.getGasPrices(chain)))
+            data.map((gasPrices, index) => {
+                app.chains[index].gasPrice = gasPrices;
+            })
+        },
+        timerCountdown: setInterval(function () {
+            app.counter--;
+            app.progressValue += 1;
+            if (app.counter === 0) {
+                app.getData();
+                // reset the values
+                app.progressValue = 0;
+                app.counter = 100;
             }
-        })
-    })
-}
-
-async function getGasPrices(network) {
-    const response = await fetch(`${endpoint}${network}`);
-    const prices = await response.json();
-    return prices;
-}
-
-async function getData() {
-    app.date = new Date().toString();
-    const data = await Promise.all(chains.map(chain => getGasPrices(chain)))
-    data.map((gasPrices, index) => {
-        app.chains[index].gasPrice = gasPrices;
-    })
-}
-
-boilerplate();
-getData();
-
-let counter = 100;
-const newYearCountdown = setInterval(function () {
-    counter--;
-    app.progressValue += 1;
-    if (counter === 0) {
-        getData();
-        // reset the values
-        app.progressValue = 0;
-        counter = 100;
+        }, 500)
     }
-}, 500);
+}
+const app = Vue.createApp(GasTracker).mount('#app');
+
+app.boilerplate()
+app.getData()
+
+
+
+
+
